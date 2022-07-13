25c334ca15fa577859e47d8eea399df37e83958b
Xiangrui Yu <1650459510@qq.com>
2022-07-13 19:25:34 +0800

    生成pk棋盘

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,14 +1,14 @@
 <template>
-    <ContentField>
-        对战
-    </ContentField> 
+    <PlayGround>
+
+    </PlayGround>
 </template>
 
 <script>
-import ContentField from "../../components/ContentField.vue"
+import PlayGround from "../../components/PlayGround.vue"
 export default {
     components: {
-        ContentField
+        PlayGround
     }
 }
 </script>
