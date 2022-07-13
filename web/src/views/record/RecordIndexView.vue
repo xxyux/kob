cf9e78b54424d53826dcf9a85b52ddc3ab0ee8eb
Xiangrui Yu <1650459510@qq.com>
2022-07-13 17:01:10 +0800

    web端导航栏

---
+++ web/src/views/record/RecordIndexView.vue
@@ -0,0 +1,18 @@
+<template>
+    <ContentField>
+        对战列表
+    </ContentField>
+</template>
+
+<script>
+import ContentField from "../../components/ContentField.vue"
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,18 @@
+<template>
+    <ContentField>
+        Bot
+    </ContentField>
+</template>
+
+<script>
+import ContentField from "../../../components/ContentField.vue"
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
