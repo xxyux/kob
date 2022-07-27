f4b2d480dc4bffa486f4a3c437160639c27dbc4a
Xiangrui Yu <1650459510@qq.com>
2022-07-27 16:50:54 +0800

    前端登录页面

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,7 +7,7 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import $ from 'jquery'
+//import $ from 'jquery'
 
 
 
@@ -16,50 +16,21 @@ export default {
     NavBar
   },
   setup() {
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/token/",
-      type: "post",
-      data:{
-        username: 'yxr',
-        password: 'pyxr',
-      },
-      success(reps) {
-        console.log(reps);
-      },
-      error(reps) {
-        console.log(reps);
-      }
-    });
-
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/info/",
-      type: "get",
-      headers: {
-        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmMmJjMjJiMjA4ZmY0MTI5YWU3Y2U2N2I0MDAwZmU4MiIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY1ODkwNjk5NSwiZXhwIjoxNjYwMTE2NTk1fQ.m8lX6SqrmY0OJoeXIuzRbs7Nx257_D1_5-kNoOVsjFM",
-      },
-      success(reps) {
-        console.log(reps);
-      },
-      error(reps) {
-        console.log(reps);
-      }
-    });
-
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/register/",
-      type: "post",
-      data:{
-        username: 'dd',
-        password: 'pdd',
-        Confpassword: 'pdd',
-      },
-      success(reps) {
-        console.log(reps);
-      },
-      error(reps) {
-        console.log(reps);
-      }
-    });
+    // $.ajax({
+    //   url:"http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data:{
+    //     username: 'dd',
+    //     password: 'pdd',
+    //     Confpassword: 'pdd',
+    //   },
+    //   success(reps) {
+    //     console.log(reps);
+    //   },
+    //   error(reps) {
+    //     console.log(reps);
+    //   }
+    // });
   }
 }
 </script>
--- web/src/views/user/account/UserAccountRegisterView.vue
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -1,12 +1,11 @@
 <template>
     <ContentField>
-        我的Bot
+        注册
     </ContentField>
 </template>
 
 <script>
-import ContentField from '../../../components/ContentField.vue'
-
+import ContentField from "../../../components/ContentField.vue"
 export default {
     components: {
         ContentField
@@ -15,4 +14,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
